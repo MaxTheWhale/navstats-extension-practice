498cb2c77163b784322d41267b0b3fb85297daa7
MaxTheWhale <maxwhale99@gmail.com>
2018-11-21 14:32:30 +0000

    Started work on OneDrive integration

--- background.js
+++ background.js
@@ -1,9 +1,52 @@
 // Load existent stats with the storage API.
-var gettingStoredStats = browser.storage.local.get();
 
-gettingStoredStats.then(results => {
+var token = "needtofigureoutauthentication" // I have just been copy/pasting auth tokens during development, need to figure out how getting tokens works
+var client = MicrosoftGraph.Client.init({
+authProvider: (done) => {
+    done(null, token); //first parameter takes an error if you can't get an access token
+}
+});
+
+// Tried to wrap getting the stats from OneDrive in a promise, so that it can be
+// used in the existing code unmodified.
+var getStats =
+  client
+    .api('/me/drive/root:/Mozilla/storage.json')
+    .get()
+      .then(fileInfo => {
+        return fetch(fileInfo["@microsoft.graph.downloadUrl"]);
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonStats => {
+        return browser.storage.local.set(jsonStats)
+      })
+      .then(res => {
+        return browser.storage.local.get();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+// At the moment this is called everytime a storage key is changed, and it
+// reuploads the entire storage json file. Need to figure out a better way.
+function uploadStats() {
+  browser.storage.local.get()
+    .then(results => {
+      return client
+        .api('/me/drive/root:/Mozilla/storage.json:/content')
+        .put(results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+getStats.then(results => {
   // Initialize the saved stats if not yet initialized.
-  if (!results.stats) {
+  if (!results.type) {
+    console.log("empty results");
     results = {
       host: {},
       type: {link: 0, reload: 0, typed: 0, generated: 0},
@@ -24,6 +67,7 @@ gettingStoredStats.then(results => {
 
     // Persist the updated stats.
     browser.storage.local.set(results);
+    uploadStats();
   });
 
   browser.webNavigation.onCompleted.addListener(evt => {
@@ -45,6 +89,7 @@ gettingStoredStats.then(results => {
 
     // Persist the updated stats.
     browser.storage.local.set(results);
+    uploadStats();
   }, {
     url: [{schemes: ["http", "https"]}]});
 });
